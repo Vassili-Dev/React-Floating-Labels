f260c6863041f8ec531769edddfc4113f112da3f
NickolaiVassiliev <Nickolai.Vassiliev@interadcorp.com>
2019-12-30 15:15:30 -0500

    Purposely failing test

--- src/components/FloatingInput/FloatingInput.test.js
+++ src/components/FloatingInput/FloatingInput.test.js
@@ -45,7 +45,7 @@ test('Label Floats then Drops on Input', () => {
        
     
         waitForDomChange(container).then(() => {
-           expect(label).toHaveClass("floating");
+           expect(label).not.toHaveClass("floating");
            waitForDomChange(container).then(() => {
                 expect(label).not.toHaveClass("floating");
                 resolve();
